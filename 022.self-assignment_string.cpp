1ff7c6aff6e76b76e6b81f73c396b3634696d86c
HubertPlonka <36955358+HubertPlonka@users.noreply.github.com>
2024-11-19 22:35:03 +0100

    022 fixed

--- 022.self-assignment_string.cpp
+++ 022.self-assignment_string.cpp
@@ -15,3 +15,62 @@ Implementation Tips
     Use this to return the current object for method chaining.
     Always ensure you manage memory properly when dealing with dynamic memory allocation.
 */
+
+#include <iostream>
+#include <cstring> 
+
+class String {
+public:
+    String(const char* str = "") {
+        size = std::strlen(str);
+        data = new char[size + 1]; 
+        std::strcpy(data, str);
+    }
+
+    ~String() {
+        delete[] data;
+    }
+
+    String(const String& other) {
+        size = other.size;
+        data = new char[size + 1];
+        std::strcpy(data, other.data);
+    }
+
+    String& operator=(const String& other) {
+        if (this == &other) {
+            return *this; 
+        }
+        delete[] data;
+
+        size = other.size;
+        data = new char[size + 1];
+        std::strcpy(data, other.data);
+
+        return *this; 
+    }
+
+    void display() const {
+        std::cout << "String: " << data << std::endl;
+    }
+
+private:
+    char* data; 
+    std::size_t size; 
+};
+
+int main() {
+    String str1("Hello");
+    String str2("World");
+
+    str1.display();
+    str2.display();
+
+    str1 = str2; 
+    str1.display();
+
+    str1 = str1; 
+    str1.display();
+
+    return 0;
+}
